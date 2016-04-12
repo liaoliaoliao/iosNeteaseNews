9ec949e542e5bc333d3972779f92ab0f9daf4b64
廖灵全 <liaolingquan@bogon>
2016-04-12 22:07:21 +0800

    初始化项目

---
+++ NeteaseNews/NeteaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeteaseNews
+//
+//  Created by 廖灵全 on 16/4/12.
+//  Copyright © 2016年 liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by 廖灵全 on 16/4/12.
+//  Copyright © 2016年 liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
